afe63bc2a9df271d679254a1290efe7d85fe36cb
Ntokozo-dev <mtsalimtsali@gmail.com>
2026-09-09 23:32:45 +0200

    Added WAV voice greeting functionality
    Adds VoiceGreeting.cs, a class that locates and plays greeting.wav
    using System.Media.SoundPlayer. Checks the file exists before
    playing, and uses PlaySync() so the greeting finishes before the
    program continues to the name prompt. greeting.wav is stored in an
    Audio folder with Build Action set to Content and Copy to Output
    Directory set to Copy if newer, so it's available at runtime.

--- CybersecurityAwarenessBot/VoiceGreeting.cs
+++ CybersecurityAwarenessBot/VoiceGreeting.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Media;
+
 
 namespace CybersecurityAwarenessBot
 {
-    internal class VoiceGreeting
+    public class VoiceGreeting
     {
+        public void PlayGreeting()
+        {
+            //locate the WAV file
+            string baseDir = AppDomain.CurrentDomain.BaseDirectory + "Audio\\greeting.wav";
+            //Check that the file exists
+            if (File.Exists(baseDir))
+            {
+                Console.WriteLine("The file exists.");
+            }
+            else
+            {
+                Console.WriteLine("The file does not exist.");
+            }
+                //Create a SoundPlayer
+                SoundPlayer playSound = new SoundPlayer(baseDir);
+            //Play the sound method
+            playSound.PlaySync();
+        }
     }
 }
+
