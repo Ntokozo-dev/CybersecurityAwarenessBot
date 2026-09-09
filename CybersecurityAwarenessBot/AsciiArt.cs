296e8b3f69aa409c2033fa784ae545b34462612b
Ntokozo-dev <mtsalimtsali@gmail.com>
2026-09-10 00:38:32 +0200

    Added cybersecurity ASCII art header

--- CybersecurityAwarenessBot/AsciiArt.cs
+++ CybersecurityAwarenessBot/AsciiArt.cs
@@ -1,12 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace CybersecurityAwarenessBot
 {
-    internal class AsciiArt
+    public class AsciiArt
     {
+        public void DisplayLogo()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("============================================");
+            Console.WriteLine("     CYBERSECURITY AWARENESS BOT            ");
+            Console.WriteLine("============================================");
+            Console.WriteLine("                   .-----.                  ");
+            Console.WriteLine("                  /  ___  \\                ");
+            Console.WriteLine("                 |  /   \\  |                ");
+            Console.WriteLine("                 | |     | |                ");
+            Console.WriteLine("                 |  \\___/  |                ");
+            Console.WriteLine("                 |---------|                ");
+            Console.WriteLine("                 |  #####  |                ");
+            Console.WriteLine("                 |  #####  |                ");
+            Console.WriteLine("                 '---------'                ");
+            Console.WriteLine("       [ ENCRYPTED ]  [ FIREWALL ]  [ SAFE ]");
+            Console.WriteLine("============================================");
+
+            Console.ResetColor();
+        }
     }
 }
